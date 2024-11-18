5d3ad435c401c6dcf52e03ac561c35aac6c8970e
diegodelgado96 <diegodelgado045@gmail.com>
2024-11-18 18:15:31 -0500

    implementación de operaciones CRUD

--- app/main.py
+++ app/main.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
+from app.models import Base, engine, Libro, SessionLocal
+from app.schemas import LibroCreate, LibroUpdate, LibroOut
 
 app = FastAPI()
 
-@app.get("/")
-def Prueba_conexión():
-    return {"message": "Bienvenido a la API de la Biblioteca"}
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/libros/", response_model=LibroOut, status_code=201)
+def agregar_libro(libro: LibroCreate, db: Session = Depends(get_db)):
+    nuevo_libro = Libro(**libro.dict())
+    db.add(nuevo_libro)
+    db.commit()
+    db.refresh(nuevo_libro)
+    return nuevo_libro
+
+@app.get("/libros/", response_model=list[LibroOut])
+def listar_libros(db: Session = Depends(get_db)):
+    return db.query(Libro).all()
